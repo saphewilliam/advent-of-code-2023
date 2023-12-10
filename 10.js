eec4b2b1b701cea86f31a81dd87fa186b7b8e9d3
William Ford <william@saphewebdesign.com>
2023-12-10 16:31:50 +0100

    Refactor Day 10 (still yikes)

--- 10.js
+++ 10.js
@@ -1,4 +1,6 @@
-const pipeDirection = 'right';
+const currentInput = input.split('\n');
+
+const PIPE_DIRECTION = 'right'; // TODO determine this dynamically
 
 function serializePoint(p) {
   return `x=${p[0]},y=${p[1]}`;
@@ -22,121 +24,86 @@ function getPipe(p) {
 }
 
 function setPipe(p, pipe) {
-  const [x, y] = p;
   if (!isInBounds(p)) return;
-  currentInput[y] =
-    currentInput[y].substring(0, x) + pipe + currentInput[y].substring(x + 1);
+  currentInput[p[1]] =
+    currentInput[p[1]].substring(0, p[0]) + pipe + currentInput[p[1]].substring(p[0] + 1);
 }
 
 function getDirection(from, to) {
-  const [fromX, fromY, toX, toY] = [...from, ...to];
-  if (fromY - toY > 0) return 'up';
-  if (toX - fromX > 0) return 'right';
-  if (toY - fromY > 0) return 'down';
-  if (fromX - toX > 0) return 'left';
+  if (from[1] - to[1] > 0) return 'up';
+  if (to[0] - from[0] > 0) return 'right';
+  if (to[1] - from[1] > 0) return 'down';
+  if (from[0] - to[0] > 0) return 'left';
 }
 
 function isValidNeighbour(from, to) {
-  const pipe = getPipe(to);
-  if (!pipe) return false;
+  if (!getPipe(to)) return false;
   return {
     up: ['S', 'F', '7', '|'],
     right: ['S', '7', 'J', '-'],
     down: ['S', 'J', 'L', '|'],
     left: ['S', 'L', 'F', '-'],
-  }[getDirection(from, to)].includes(pipe);
+  }[getDirection(from, to)].includes(getPipe(to));
 }
 
+const up = (p) => [p[0], p[1] - 1];
+const right = (p) => [p[0] + 1, p[1]];
+const down = (p) => [p[0], p[1] + 1];
+const left = (p) => [p[0] - 1, p[1]];
+const walk = (p, direction) => direction(p);
+
 function getNeighbours(p) {
-  const [x, y] = p;
   return (() => {
     switch (getPipe(p)) {
       case '|':
-        return [
-          [x, y - 1],
-          [x, y + 1],
-        ];
+        return [up, down];
       case '-':
-        return [
-          [x - 1, y],
-          [x + 1, y],
-        ];
+        return [left, right];
       case 'L':
-        return [
-          [x, y - 1],
-          [x + 1, y],
-        ];
+        return [up, right];
       case 'J':
-        return [
-          [x, y - 1],
-          [x - 1, y],
-        ];
+        return [up, left];
       case '7':
-        return [
-          [x, y + 1],
-          [x - 1, y],
-        ];
+        return [down, left];
       case 'F':
-        return [
-          [x, y + 1],
-          [x + 1, y],
-        ];
+        return [down, right];
       case 'S':
-        return [
-          [x, y - 1],
-          [x + 1, y],
-          [x, y + 1],
-          [x - 1, y],
-        ];
+        return [up, down, left, right];
       default:
         return [];
     }
-  })().filter((n) => isValidNeighbour(p, n));
+  })()
+    .map((dir) => walk(p, dir))
+    .filter((n) => isValidNeighbour(p, n));
 }
 
-function setLeftRight(point, direction) {
-  const [x, y] = point;
-  const [left, right] = {
-    up: [
-      [x - 1, y],
-      [x + 1, y],
-    ],
-    left: [
-      [x, y + 1],
-      [x, y - 1],
-    ],
-    down: [
-      [x + 1, y],
-      [x - 1, y],
-    ],
-    right: [
-      [x, y - 1],
-      [x, y + 1],
-    ],
-  }[direction];
+function setLeftRight(p, direction) {
+  const [leftP, rightP] = {
+    up: [left, right],
+    left: [down, up],
+    down: [right, left],
+    right: [up, down],
+  }[direction].map((dir) => walk(p, dir));
 
-  if (getPipe(left) === '.') setPipe(left, pipeDirection === 'left' ? 'I' : 'O');
-  if (getPipe(right) === '.') setPipe(right, pipeDirection === 'left' ? 'O' : 'I');
+  if (getPipe(leftP) === '.') setPipe(leftP, PIPE_DIRECTION === 'left' ? 'I' : 'O');
+  if (getPipe(rightP) === '.') setPipe(rightP, PIPE_DIRECTION === 'left' ? 'O' : 'I');
 }
-
-const currentInput = input.split('\n');
 
 const startPoint = currentInput.reduce(
   (p, c, i) => (c.indexOf('S') !== -1 ? [c.indexOf('S'), i] : p),
   null,
 );
 
-const pathSet = new Set([serializePoint(startPoint)]);
-const stack = getNeighbours(startPoint);
-
 // Part 1: DFS traverse, record the path and its length
 let steps = 0;
+const pathDiscovered = new Set([serializePoint(startPoint)]);
+const stack = getNeighbours(startPoint);
 while (stack.length > 0) {
-  const item = stack.pop();
-  if (getPipe(item) === 'S' && steps > 1) break;
-  if (!pathSet.has(serializePoint(item))) {
-    pathSet.add(serializePoint(item));
-    for (const n of getNeighbours(item)) stack.push(n);
+  const point = stack.pop();
+  if (getPipe(point) === 'S' && steps > 1) break;
+  if (!pathDiscovered.has(serializePoint(point))) {
+    pathDiscovered.add(serializePoint(point));
+    for (const n of getNeighbours(point)) stack.push(n);
     steps++;
   }
 }
@@ -144,10 +111,10 @@ while (stack.length > 0) {
 // Set all points that are not part of the main loop to '.'
 for (let y = 0; y < currentInput.length; y++)
   for (let x = 0; x < currentInput[y].length; x++)
-    if (!pathSet.has(serializePoint([x, y]))) setPipe([x, y], '.');
+    if (!pathDiscovered.has(serializePoint([x, y]))) setPipe([x, y], '.');
 
 // Part 2: Traverse path again, and set all outside '.' to O and all inside '.' to I
-const path = [...pathSet.entries()].map((p) => deserializePoint(p));
+const path = [...pathDiscovered.entries()].map((p) => deserializePoint(p));
 for (let i = 0; i < path.length; i++) {
   const point = path[i];
   if (i !== 0) setLeftRight(point, getDirection(path[i - 1], point));
@@ -157,31 +124,28 @@ for (let i = 0; i < path.length; i++) {
 // DFS fill all remaining '.' with either O or I
 for (let y = 0; y < currentInput.length; y++) {
   for (let x = 0; x < currentInput[y].length; x++) {
-    const p = [x, y];
-    if (getPipe(p) === '.') {
-      const discovered = new Set([serializePoint(p)]);
-      const queue = [p];
+    const source = [x, y];
+    if (getPipe(source) === '.') {
+      const discovered = new Set([serializePoint(source)]);
+      const queue = [source];
       let io = null;
       while (queue.length > 0) {
-        const item = queue.shift();
-        for (const n of [
-          [item[0], item[1] - 1],
-          [item[0] + 1, item[1]],
-          [item[0], item[1] - 1],
-          [item[0] - 1, item[1]],
-        ].filter((n) => {
-          pipe = getPipe(n);
-          if (pipe === 'I') io = 'I';
-          if (pipe === 'O') io = 'O';
-          return !discovered.has(serializePoint(n)) && isInBounds(n) && pipe === '.';
-        })) {
+        const point = queue.shift();
+        for (const n of [up, down, left, right]
+          .map((dir) => walk(point, dir))
+          .filter((n) => {
+            pipe = getPipe(n);
+            if (pipe === 'I') io = 'I';
+            if (pipe === 'O') io = 'O';
+            return !discovered.has(serializePoint(n)) && isInBounds(n) && pipe === '.';
+          })) {
           discovered.add(serializePoint(n));
           queue.push(n);
         }
       }
 
       const points = [...discovered.entries()].map((p) => deserializePoint(p));
-      for (const point of points) setPipe(point, io);
+      for (const p of points) setPipe(p, io);
     }
   }
 }
